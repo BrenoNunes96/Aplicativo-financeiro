02ee4c716a6056a347456aba15588a65b67055cd
BrenoNunes96 <brenocp3@live.com>
2025-05-27 13:38:00 -0300

    Atualiza projeto com novas alterações

--- src/Data/firebaseConnetcion.js
+++ src/Data/firebaseConnetcion.js
@@ -1,11 +1,9 @@
-// Import the functions you need from the SDKs you need
+// Importações do SDK Firebase necessárias
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";  // <-- import do Realtime Database
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuração do Firebase (sua config)
 const firebaseConfig = {
   apiKey: "AIzaSyCQIKqnPlpdz4lusuPEzoLnGJF0LpnfxNw",
   authDomain: "hpproject-65d6d.firebaseapp.com",
@@ -17,6 +15,14 @@ const firebaseConfig = {
   measurementId: "G-RKYEGXSLT9"
 };
 
-// Initialize Firebase
+// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
+
+// Inicializa Analytics
 const analytics = getAnalytics(app);
+
+// Inicializa Realtime Database
+const realtimeDb = getDatabase(app);  // <-- aqui
+
+// Exporta o realtimeDb para usar em outros arquivos
+export { realtimeDb };
